94192f4ad7b384f87d54a5927321b78586ad5e10
Learner <cchukwudi099@gmail.com>
2026-01-05 21:51:40 +0100

    styled all the cards in the vans page and ready them for hrouping for their personal description

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,6 @@ function App() {
         <Route path='/about' element={<About />} />
         <Route path='/vans' element={<VansPage />} />
       </Routes>
-      {/* <VansPage/> */}
     </>
 
   )
--- src/component/pages/Vans.Page.jsx
+++ src/component/pages/Vans.Page.jsx
@@ -4,15 +4,15 @@ import clsx from "clsx"
 const Card = (props) => {
     const { imgLink, name, price, type } = props
     return (
-        <section className={clsx("card")}>
-            <figure>
-                <img src={imgLink} alt={name} />
+        <section className={clsx("card", "flex flex-col gap-[10px] ")}>
+            <figure className="w-[300px] max-[20rem]:w-[230px] h-[200px] ">
+                <img className ={"h-[100%] w-[100%] "} src={imgLink} alt={name} />
             </figure>
-            <article>
-                <p>{name}</p>
-                <span>{price} <br />/day</span>
+            <article className="flex justify-between ">
+                <p className="text-green-600 font-bold">{name}</p>
+                <span><code className="font-bold">{price}</code> <br />/day</span>
             </article>
-            <code>{type}</code>
+            <code className={clsx("px-4 py-2 rounded-md bg-black w-fit text-white ")}>{type}</code>
         </section>
     )
 }
@@ -30,8 +30,11 @@ const VansPage = () => {
 
     return (
         <>
-            <main className="vansPage">
-                {newElements}
+            <main className={clsx("vansPage", "flex flex-wrap gap-[20px] p-4 justify-center items-center")}>
+                <h3 className="text-3xl font-bold"><span className="text-blue-400">Explore</span> our van options</h3>
+                <section className={clsx("flex flex-wrap gap-[20px] p-4 justify-center items-center")}>
+                    {newElements}
+                </section>
             </main>
         </>
     )
